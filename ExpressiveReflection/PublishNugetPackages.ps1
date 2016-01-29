53d4bd899793f9959a338837e5fa3c64c5f150a8
hannasm <hannasm-commit>
2016-01-28 19:36:42 -0500

    Changes To Publishing Script
    Publishing script needs to push the symbols package too
    Publishing script needs to wrap string concats in quotes for write-error

--- ExpressiveReflection/PublishNugetPackages.ps1
+++ ExpressiveReflection/PublishNugetPackages.ps1
@@ -2,17 +2,22 @@
 $version = $data.package.metadata.version;
 
 $assemblyPackage = 'ExpressiveReflection.' + $version + '.nupkg';
+$symbolsPackage = 'ExpressiveReflection.' + $version + '.symbols.nupkg';
 $sourcePackage = 'ExpressiveReflection.Sources.' + $version + '.nupkg';
 
-if (!(test-path $assemblyPackage) -or !(Test-Path $sourcePackage)) {
+if (!(test-path $assemblyPackage) -or !(Test-Path $sourcePackage) -or !(Test-Path $symbolsPackage)) {
 	if (!(test-path $assemblyPackage)) {
-		Write-Error 'Unable teo find assembly package ' + $assemblyPackage;
+		Write-Error ('Unable teo find assembly package ' + $assemblyPackage);
 	}
 	if (!(Test-Path $sourcePackage)) {
-		Write-Error 'Unable to find source package ' + $sourcePackage;
+		Write-Error ('Unable to find source package ' + $sourcePackage);
+	}
+	if (!(Test-Path $symbolsPackage)) {
+		Write-Error ('Unable to find symbols package ' + $symbolsPackage);
 	}
 	return;
 } else {
 	nuget push $assemblyPackage;
+	nuget push $symbolsPackage;
 	nuget push $sourcePackage;
 }
